7473b298a4dcdc1a551b951d99a0e9b1e99f3974
Yasmin Nogueira Spadaro Cropanisi <yasmincropanisi@gmail.com>
2017-08-26 20:56:53 -0300

    Associar cartão a lista de usuários/ buscar cartão com lista de usuários

--- TeenCard/TeenCard/Card.swift
+++ TeenCard/TeenCard/Card.swift
@@ -7,9 +7,32 @@
 //
 
 import UIKit
+import CloudKit
 
 class Card:NovoCartao {
 
-   
+    var record: CKRecord!
     var history:History?
+    var id:Int?
+    var blocked:Int?
+    var number:String?
+    var users: [User]?
+
+    
+    
+    init(withRecord record: CKRecord) {
+        super.init()
+        self.record = record
+        self.id = record["id"] as? Int ?? 0
+        self.number = record["number"] as? String
+        self.contrasenha = record["countersign"] as? String
+        self.valor = record["found"] as? Double
+        self.blocked = record["blocked"] as? Int ?? 0
+        self.idCartao = record["number"] as? String
+
+        
+    }
+    init(number:String) {
+        self.number = number
+    }
 }
